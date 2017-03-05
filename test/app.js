e31c516e28ed59985813818968cad769a9061735
Vishruth Krishna Prasad <krishnaprasad.v@husky.neu.edu>
2017-03-05 18:48:08 -0500

    added package

---
+++ test/app.js
@@ -0,0 +1,67 @@
+module.exports = function(app)
+{
+    app.get("/api/test", findAllMessages);
+    app.post("/api/test", createMessage);
+    app.delete("/api/test/:id", deleteMessage);
+
+ 
+ 	if(process.env.MONGODB_URI){
+    	connectionString = process.env.MONGODB_URI
+ 	}
+ 	//var connectionString = 'mongodb://someone:somepassword@ds111589.mlab.com:11589/firstone'
+    if(process.env.MLAB_USERNAME) {
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
+    }
+
+    var mongoose = require("mongoose");
+    mongoose.connect(connectionString);
+
+    var TestSchema = mongoose.Schema({
+        message: String
+    });
+
+    var TestModel = mongoose.model("TestModel", TestSchema);
+
+    function findAllMessages(req, res) {
+        TestModel
+            .find()
+            .then(
+                function(tests) {
+                    res.json(tests);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+
+    function createMessage(req, res) {
+        TestModel
+            .create(req.body)
+            .then(
+                function(test) {
+                    res.json(test);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+
+    function deleteMessage(req, res) {
+        TestModel
+            .remove({_id: req.params.id})
+            .then(
+                function(result) {
+                    res.json(result);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+};
