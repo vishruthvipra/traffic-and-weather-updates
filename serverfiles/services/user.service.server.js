20957b65b42cfd688383a602d7c3e3a19137802b
Vishruth Krishna Prasad <krishnaprasad.v@husky.neu.edu>
2017-04-14 20:59:14 -0400

    maybe https corrected

--- serverfiles/services/user.service.server.js
+++ serverfiles/services/user.service.server.js
@@ -30,17 +30,17 @@ module.exports = function (app, model, passport) {
 
     app.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
     app.get('/auth/facebook/callback', passport.authenticate('facebook', {
-        failureRedirect: 'http://webdevproject.herokuapp.com/project/#/home'
+        failureRedirect: 'https://webdevproject.herokuapp.com/project/#/home'
     }), function (req, res) {
-        var url = 'http://webdevproject.herokuapp.com/project/#/dashboard';
+        var url = 'https://webdevproject.herokuapp.com/project/#/dashboard';
         res.redirect(url);
     });
 
     app.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
     app.get('/auth/google/callback', passport.authenticate('google', {
-        failureRedirect: 'http://webdevproject.herokuapp.com/project/#/home'
+        failureRedirect: 'https://webdevproject.herokuapp.com/project/#/home'
     }), function (req, res) {
-            var url = 'http://webdevproject.herokuapp.com/project/#/dashboard';
+            var url = 'https://webdevproject.herokuapp.com/project/#/dashboard';
             res.redirect(url);
         });
 
